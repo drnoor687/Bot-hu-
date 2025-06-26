4f586854b9fe6634eb1cbb793b2b22b7f2deda7e
AshishVerma340 <januverma558@gmail.com>
2025-06-26 13:24:12 +0530

    Update Ice cream.js

--- Priyansh/commands/Ice cream.js
+++ Priyansh/commands/Ice cream.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("ICE CREAM")==0 || event.body.indexOf("Ice Cream")==0 || event.body.indexOf("Ice cream")==0 || event.body.indexOf("Icecream")==0) {
 		var msg = {
-				body: "💝 YE LO BBY ICE CREAM KHAO 🍨\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲༄𒁍≛⃝𝐃𝐫.𝐌𝐮𝐬𝐤𝐚𝐧❤️-)",
+				body: "💝 YE LO BBY ICE CREAM KHAO 🍨\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲༄𒁍≛⃝MR.ASHISH❤️-)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/Icecream.jpg`)
 			}
