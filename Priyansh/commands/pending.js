4c177db8b2f4ce1a69cd3df04e6cb5d2354640de
AshishVerma340 <januverma558@gmail.com>
2025-07-07 16:39:18 +0530

    Update pending.js

--- Priyansh/commands/pending.js
+++ Priyansh/commands/pending.js
@@ -22,7 +22,7 @@ module.exports.languages = {
     "en": {
         "invaildNumber": "%1 is not an invalid number",
         "cancelSuccess": "Refused %1 thread!",
-        "notiBox": "Mahi BoT Connected Successfully!\nUse +help for more info :>",
+        "notiBox": "ASHISH.VERMA BoT Connected Successfully!\nUse +help for more info :>",
         "approveSuccess": "Approved successfully %1 threads!",
 
         "cantGetPendingList": "Can't get the pending list!",
