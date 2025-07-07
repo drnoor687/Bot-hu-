7df13ee47e4d7595fa508aeb951dd95191f7b89f
AshishVerma340 <januverma558@gmail.com>
2025-07-07 16:08:07 +0530

    Update owner.js

--- Priyansh/commands/owner.js
+++ Priyansh/commands/owner.js
@@ -20,7 +20,7 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
     var link = [
-"https://l.facebook.com/l.php?u=jpg"
+"[FB-IMG-1751655116411.jpg]"
      ];
      var callback = () => api.sendMessage({body:`🔰𝙊𝙒𝙉𝙀𝙍 𝙄𝙉𝙁𝙊🔰
 
